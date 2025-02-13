235e4f2c997f787c1b6094a06143713968844467
jeromegsa <jeromegbossa7@gmail.com>
2025-02-13 20:02:48 +0100

    mise à jour

--- database.py
+++ database.py
@@ -2,14 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DataBase_URL="postgresql:://postgres:00000000@localhost/animaux_db"
+DATABASE_URL = "postgresql://postgres:tonmotdepasse@localhost/animaux_db"
 
 # create an instance of connexion engine
 
-engine=create_engine(DataBase_URL)
+engine=create_engine(DATABASE_URL)
 
 # creer une session pour interagir avec la base
-SessionLocal=sessionmaker(autocommit=False, autoflush=false, bind=engine)
+SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base pour définir les modèles 
 Base =declarative_base()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-import models
+import database
 import crud
 import database
 
