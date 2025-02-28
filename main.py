9f4b4748d6c501aaed17bdf970d88256e2808a0f
Jeff_Gsa <jeromegbossa7@gmail.com>
2025-03-01 00:05:38 +0100

    mise à jour

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
-from routers import auth
+from routers import auth, user_router
 from database.connection import engine, create_tables, reset_db
 
 app = FastAPI()
 
 # Inclure le routeur d'authentification
 app.include_router(auth.router)
+#inclure le routeur des utilisateurs 
+app.include_router(user_router.router)
 
 @app.on_event("startup")
 def on_startup():
-    reset_db()
-    create_tables()
+    # reset_db()
+    # create_tables()
     print("startup")
 
 @app.get("/")
--- routers/user_router.py
+++ routers/user_router.py
@@ -1,6 +1,15 @@
 from fastapi import APIRouter, Depends
-from  models import User
+from  models import User,UserCreate
+from sqlmodel import Session
+from database import get_session
+from  crud import store
 
 router=APIRouter()
 
-# @router.post("/users/", response_model=User)
+@router.post("/users/", response_model=User)
+def create_user(user: UserCreate, session: Session = Depends(get_session)):
+    """
+    Crée un nouvel utilisateur.
+    """
+    db_user = store( user, session)
+    return db_user
