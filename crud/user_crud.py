b33d8c7ca83cd3b741ffce48c347efc0dba8eb4f
jeromegsa <jeromegbossa7@gmail.com>
2025-02-28 18:09:34 +0100

    affichage d'un user par son id

--- crud/user_crud.py
+++ crud/user_crud.py
@@ -11,33 +11,35 @@ def store(user_data : UserCreate, session : Session =Depends (get_session)):
     """
     Crée un nouvel utilisateur après validation 
     """
-    
-    # Vérifier si l'utilisateur existe déjà 
-    existing_user= session.query(User).filter(User.username==user_data.username).first()
-    
-    if existing_user:
-        raise HTTPException(status_code=400, detail="L'utlisateur existe déjà ")
-    
-    #hasher le mot de passe 
-    hashed_password=pwd_context.hash(user_data.password)
-    
-    #Créeer un nouvel objet utilisateur 
-    
-    new_user=User(
-        username=user_data.username,
-        nom =user_data.nom,
-        prenom= user_data.prenom,
-        email=user_data.email,
-        password=hashed_password,
-        localisation=user_data.localisation,
-        role=user_data.role
-)
-    # Ajouter et enregistrer dans la base de données 
+    try:
+        # Vérifier si l'utilisateur existe déjà 
+        existing_user= session.query(User).filter(User.username==user_data.username).first()
+        
+        if existing_user:
+            raise HTTPException(status_code=400, detail="L'utlisateur existe déjà ")
+        
+        #hasher le mot de passe 
+        hashed_password=pwd_context.hash(user_data.password)
+        
+        #Créeer un nouvel objet utilisateur 
+        
+        new_user=User(
+            username=user_data.username,
+            nom =user_data.nom,
+            prenom= user_data.prenom,
+            email=user_data.email,
+            password=hashed_password,
+            localisation=user_data.localisation,
+            role=user_data.role
+    )
+        # Ajouter et enregistrer dans la base de données 
 
-    session.add(new_user)
-    session.commit()
-    session.refresh(new_user)
-    return new_user
+        session.add(new_user)
+        session.commit()
+        session.refresh(new_user)
+        return new_user
+    except HTTPException as e :
+        print (e)
 
 def get_users(session: Session = Depends(get_session),current_user: User=Depends(get_current_user)):
     """
@@ -48,4 +50,33 @@ def get_users(session: Session = Depends(get_session),current_user: User=Depends
         return users
     except HTTPException as e :
         print (e)
-
+        
+def get_user_by_id(user_id, session: Session = Depends(get_session), current_user: User=Depends(get_current_user)):
+    """
+    Récupère un utilisateur par son ID de la base de données
+    """
+    try:
+        user_id=int(user_id)
+        user=session.query(User).filter(User.id==user_id).first()
+        if not user:
+            raise HTTPException(status_code=404, detail="Utilisateur introuvable")
+        return user
+    except HTTPException as e :
+        print (e)
+        
+def  delete_user(user_id:int , session: Session = Depends(get_session), current_user: User=Depends(get_current_user)):
+    """
+    Supprime un utilisateur de la base de données
+    """
+    try:
+        user_id=int(user_id)
+        user=session.query(User).filter(User.id==user_id).first()
+        if not user:
+            raise HTTPException(status_code=404, detail="Utilisateur introuvable")
+        session.delete(user)
+        session.commit()
+        return {"message": "Utilisateur supprimé"}
+    except HTTPException as e :
+        print (e)
+        
+    
--- routers/user_router.py
+++ routers/user_router.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends,HTTPException 
 from models import User, UserCreate
 from sqlmodel import Session
 from database import get_session
-from crud import store,get_users
+from crud import store,get_users,get_user_by_id,delete_user
 from dependencies import get_current_user
 
 router = APIRouter()
@@ -26,3 +26,17 @@ def getAlluser(session: Session = Depends(get_session), current_user: User=Depen
     """
     users =  get_users(session, current_user)
     return users
+
+@router.get("/users/{user_id}")
+def getUserById(user_id:int, session: Session = Depends(get_session), current_user: User=Depends(get_current_user)):
+    """
+    Recupère un utilisateur par son id
+    """
+    user = get_user_by_id(user_id, session, current_user)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+@router.delete("/users")
+def  delete_one_user(user_id:int , session: Session = Depends(get_session), current_user: User=Depends(get_current_user)):
+    message_suppression=delete_user(user_id,session, current_user)
+    return {"message": message_suppression}
