e3c36096c9cb55022892b8f02d5f61053becccf1
jeromegsa <jeromegbossa7@gmail.com>
2025-03-06 18:24:41 +0100

    contact avec le front

--- zoo_front/src/App.jsx
+++ zoo_front/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import api from "./api/axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token"); // Récupérer le token stocké
+
+    api.get("/users/", {
+      headers: {
+        Authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHJpbmciLCJyb2xlIjoiYWRtaW4iLCJleHAiOjE3NDEyODM0Njl9.UgemZloFfX8QfZ_wk2alUsoAEQBbk0-44wE2mOfbBIY`,
+      },
+    })
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la récupération des utilisateurs:", error);
+      });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Liste des utilisateurs</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.nom} - {user.email}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
